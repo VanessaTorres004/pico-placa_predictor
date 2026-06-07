efe408124b0fd9a06076928db5ced3a4e33a81c3
Vanessa <vtorres2004.1211@gmail.com>
2026-06-07 11:47:16 -0500

    actualizacion day of week

--- pico_placa/enums/day_of_week.py
+++ pico_placa/enums/day_of_week.py
@@ -2,27 +2,16 @@ from enum import Enum
 
 
 class DayOfWeek(Enum):
-    MONDAY = 1
-    TUESDAY = 2
+    MONDAY    = 1
+    TUESDAY   = 2
     WEDNESDAY = 3
-    THURSDAY = 4
-    FRIDAY = 5
-    SATURDAY = 6
-    SUNDAY = 7
+    THURSDAY  = 4
+    FRIDAY    = 5
+    SATURDAY  = 6
+    SUNDAY    = 7
 
     def is_weekend(self) -> bool:
         return self in (DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
 
-    def restricted_digits(self) -> tuple[int, ...]:
-        restrictions = {
-            DayOfWeek.MONDAY: (1, 2),
-            DayOfWeek.TUESDAY: (3, 4),
-            DayOfWeek.WEDNESDAY: (5, 6),
-            DayOfWeek.THURSDAY: (7, 8),
-            DayOfWeek.FRIDAY: (9, 0),
-        }
-
-        return restrictions.get(self, ())
-
-    def has_restriction(self, plate_last_digit: int) -> bool:
-        return plate_last_digit in self.restricted_digits()
+    def label(self) -> str:
+        return self.name.capitalize()
